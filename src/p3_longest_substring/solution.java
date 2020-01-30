c54b6f39b4cf11039307c6061feee3e4ea2f140c
lixinfeng <1072153336@qq.com>
2020-01-30 23:25:43 +0800

    首次解决最长不重复子串。

---
+++ src/p3_longest_substring/solution.java
@@ -0,0 +1,36 @@
+package p3_longest_substring;
+
+import java.lang.reflect.Array;
+import java.util.*;
+
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        Queue<Character> longestSubstring = new LinkedList<>();
+        Queue<Character> checkedSubstring = new LinkedList<>();
+        Map<Character, Integer> checkedMap = new HashMap<>();
+        for (int i = 0; i < s.length(); i++) {
+            Character c = s.charAt(i);
+            Integer index = checkedMap.get(c);
+            if (index == null) {
+                checkedMap.put(c, checkedSubstring.size());
+                checkedSubstring.offer(c);
+                if (checkedSubstring.size() > longestSubstring.size()) {
+                    longestSubstring = new LinkedList<>(checkedSubstring);
+                }
+            } else {
+                while (c != checkedSubstring.peek()) {
+                    checkedMap.remove(checkedSubstring.poll());
+                }
+                checkedSubstring.poll();
+                checkedSubstring.offer(c);
+            }
+        }
+
+        return longestSubstring.size();
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.lengthOfLongestSubstring("pwwb"));
+    }
+}
