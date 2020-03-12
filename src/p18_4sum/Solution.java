d1313497b4a2b4e3f74a92df515863f93ac399a0
lixinfeng <lixinfeng@dm-ai.cn>
2020-03-12 23:00:27 +0800

    #18 完成四数之和

--- src/p18_4sum/Solution.java
+++ src/p18_4sum/Solution.java
@@ -10,32 +10,39 @@ class Solution {
         List<List<Integer>> result = new ArrayList <>();
         if (nums.length < 4) return new ArrayList<>();
         int p1 = 0, p2 = 1, p3 = 2, p4 = nums.length-1;
-        while (p1 < nums.length) {
-            while (p2 < nums.length) {
+        while (p1 < nums.length-3) {
+            int num1 = nums[p1];
+            while (p2 < nums.length-2) {
+                int num2 = nums[p2];
                 while (p3 < p4) {
                     int sum = nums[p1] + nums[p2] + nums[p3] + nums[p4];
+                    int num3 = nums[p3];
+                    int num4 = nums[p4];
                     if (sum == target) {
                         result.add(Arrays.asList(nums[p1], nums[p2], nums[p3], nums[p4]));
-                        p3 = p3 + 1;
-                        p4 = p4 - 1;
+                        while (++p3 < p4 && nums[p3] == num3){}
+                        while (--p4 > p3 && nums[p4] == num4){}
                     } else if (sum < target) {
-                        p3 = p3 + 1;
+                        while (++p3 < p4 && nums[p3] == num3){}
                     } else {
-                        p4 = p4 - 1;
+                        while (--p4 > p3 && nums[p4] == num4){}
                     }
                 }
-                p2 ++;
+                while (++p2 < nums.length && num2 == nums[p2]){}
                 p3 = p2 + 1;
                 p4 = nums.length - 1;
             }
-            p1 ++;
+
+            while (++p1 < nums.length && num1 == nums[p1]){}
             p2 = p1 + 1;
+            p3 = p2 + 1;
+            p4 = nums.length - 1;
         }
         return result;
     }
 
     public static void main(String[] args) {
         Solution solution = new Solution();
-        System.out.println(solution.fourSum(new int[]{-2, -1, 0, 0, 1, 2}, 0).toString());
+        System.out.println(solution.fourSum(new int[]{-2, -1, -1, 0, 0, 1, 2}, 0).toString());
     }
 }
