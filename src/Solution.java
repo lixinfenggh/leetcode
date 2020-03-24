0b0066a240f0c7a7c787feaa605697f6fe343696
lixinfeng <lixinfeng@dm-ai.cn>
2020-03-24 10:48:14 +0800

    面试题 17.16. 按摩师

--- src/Solution.java
+++ src/Solution.java
@@ -1,26 +1,18 @@
-class ListNode {
-    private int val;
-    ListNode next;
-    ListNode(int x) { val = x; }
-}
-
 public class Solution {
-    public ListNode middleNode(ListNode head) {
-        if (head == null) return null;
-        ListNode oneStep = head;
-        ListNode twoStep = head;
-        boolean towStepForward = false;
-        while (oneStep != null) {
-            oneStep = oneStep.next;
-            if (towStepForward) {
-                twoStep = twoStep.next;
-            }
-            towStepForward = !towStepForward;
+    public int massage(int[] nums) {
+        if (nums.length == 0) return 0;
+        if (nums.length == 1) return nums[0];
+        if (nums.length == 2) return Math.max(nums[0], nums[1]);
+        int maxLength1 = nums[0], maxLength2 = Math.max(nums[0], nums[1]);
+        for (int i = 2; i < nums.length; i++) {
+            int old1 = maxLength1;
+            maxLength1 = maxLength2;
+            maxLength2 = Math.max(old1+nums[i], maxLength2);
         }
-        return twoStep;
+        return Math.max(maxLength1, maxLength2);
     }
 
     public static void main(String[] args) {
-        System.out.println(new Solution());
+        System.out.println(new Solution().massage(new int[] {1, 3, 1, 3, 100}));
     }
 }
