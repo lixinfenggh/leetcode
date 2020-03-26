4f98354aa6c2078444577e81a918462df583343e
lixinfeng <lixinfeng@dm-ai.cn>
2020-03-26 22:40:15 +0800

    999. 车的可用捕获量

--- src/Solution.java
+++ src/Solution.java
@@ -1,24 +1,49 @@
 public class Solution {
-    public int surfaceArea(int[][] grid) {
-        if (grid.length == 1 && grid[0].length == 0) return 0;
-        int boxNum = 0;
-        int cover = 0;
-        for (int i = 0; i < grid.length; i++) {
-            for (int j = 0; j < grid[i].length; j++) {
-                if (i > 0)
-                    cover += Math.min(grid[i][j], grid[i-1][j]);
-                if (j > 0)
-                    cover += Math.min(grid[i][j], grid[i][j-1]);
-                if (grid[i][j] > 0)
-                    cover += grid[i][j] - 1;
-                boxNum += grid[i][j];
+    public int numRookCaptures(char[][] board) {
+        int x=-1, y=-1;
+        mainLoop:
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[i].length; j++) {
+                if (board[i][j] == 'R') {
+                    x = i;
+                    y = j;
+                    break mainLoop;
+                }
             }
         }
-
-        return boxNum * 6 - cover * 2;
+        int result = 0;
+        for (int i = x-1; i > 0; i--) {
+            if (board[i][y] == 'B') break;
+            if (board[i][y] == 'p') {
+                result ++;
+                break;
+            }
+        }
+        for (int i = x+1; i < board.length; i++) {
+            if (board[i][y] == 'B') break;
+            if (board[i][y] == 'p') {
+                result ++;
+                break;
+            }
+        }
+        for (int i = y-1; i > 0; i--) {
+            if (board[x][i] == 'B') break;;
+            if (board[x][i] == 'p') {
+                result++;
+                break;
+            }
+        }
+        for (int i = y+1; i < board[x].length; i++) {
+            if (board[x][i] == 'B') break;;
+            if (board[x][i] == 'p') {
+                result++;
+                break;
+            }
+        }
+        return result;
     }
 
     public static void main(String[] args) {
-        System.out.println(new Solution().surfaceArea(new int[][] {{1, 1, 1}, {1, 0, 1}, {1, 1, 1}}));
+        System.out.println(new Solution().numRookCaptures(new char[][] {}));
     }
 }
