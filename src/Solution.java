48b399af0a54ce86af624f9656d800542b536dd3
lixinfeng <lixinfeng@dm-ai.cn>
2020-03-27 01:39:19 +0800

    914. 卡牌分组

--- src/Solution.java
+++ src/Solution.java
@@ -1,49 +1,23 @@
-public class Solution {
-    public int numRookCaptures(char[][] board) {
-        int x=-1, y=-1;
-        mainLoop:
-        for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board[i].length; j++) {
-                if (board[i][j] == 'R') {
-                    x = i;
-                    y = j;
-                    break mainLoop;
-                }
+class Solution {
+    public boolean hasGroupsSizeX(int[] deck) {
+        int[] count = new int[10000];
+        for (int c: deck)
+            count[c]++;
+
+        int g = -1;
+        for (int i = 0; i < 10000; ++i) {
+            if (count[i] > 0) {
+                if (g == -1)
+                    g = count[i];
+                else
+                    g = gcd(g, count[i]);
             }
         }
-        int result = 0;
-        for (int i = x-1; i > 0; i--) {
-            if (board[i][y] == 'B') break;
-            if (board[i][y] == 'p') {
-                result ++;
-                break;
-            }
-        }
-        for (int i = x+1; i < board.length; i++) {
-            if (board[i][y] == 'B') break;
-            if (board[i][y] == 'p') {
-                result ++;
-                break;
-            }
-        }
-        for (int i = y-1; i > 0; i--) {
-            if (board[x][i] == 'B') break;;
-            if (board[x][i] == 'p') {
-                result++;
-                break;
-            }
-        }
-        for (int i = y+1; i < board[x].length; i++) {
-            if (board[x][i] == 'B') break;;
-            if (board[x][i] == 'p') {
-                result++;
-                break;
-            }
-        }
-        return result;
+
+        return g > 1;
     }
 
-    public static void main(String[] args) {
-        System.out.println(new Solution().numRookCaptures(new char[][] {}));
+    public int gcd(int x, int y) {
+        return x == 0 ? y : gcd(y%x, x);
     }
 }
