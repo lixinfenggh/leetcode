a208b28beef213eca657df83b77106f0de9c166a
lixinfeng <lixinfeng@dm-ai.cn>
2020-03-29 20:26:31 +0800

    1162. 地图分析

--- src/Solution.java
+++ src/Solution.java
@@ -1,41 +1,48 @@
-import java.util.ArrayList;
-import java.util.List;
-
 class Solution {
-    private String reverse(String word) {
-        return new StringBuilder(word).reverse().toString();
-    }
-
-    public int minimumLengthEncoding(String[] words) {
-        List<String> result = new ArrayList<String>();
-        result.add(reverse(words[0]));
-        for (int i = 1; i < words.length; i++) {
-            boolean in = false;
-            String rw = reverse(words[i]);
-            for (int j = 0; j < result.size(); j++) {
-                if (result.get(j).length() > rw.length()) {
-                    if (result.get(j).substring(0, rw.length()).equals(rw)) {
-                        in = true;
-                        break;
-                    }
-                } else if (rw.substring(0, result.get(j).length()).equals(result.get(j))) {
-                    in = true;
-                    result.set(j, rw);
-                    break;
+    public int maxDistance(int[][] grid) {
+        boolean needToScan = false;
+        int[][] dis = new int[grid.length][grid[0].length];
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[i].length; j++) {
+                if (grid[i][j] == 1) {
+                    dis[i][j] = 0;
+                } else {
+                    dis[i][j] = -1;
                 }
             }
-            if (!in)
-                result.add(rw);
         }
-        int sum = 0;
-        for (int i = 0; i < result.size(); i++) {
-            sum += result.get(i).length();
+        int scan = 0;
+        do {
+            needToScan = false;
+            for (int i = 0; i < dis.length; i++) {
+                for (int j = 0; j < dis[i].length; j++) {
+                    if (dis[i][j] == scan) {
+                        if (i-1 >= 0 && dis[i-1][j] == -1)
+                            dis[i-1][j] = scan+1;
+                        if (i+1 < dis.length && dis[i+1][j] == -1)
+                            dis[i+1][j] = scan+1;
+                        if (j-1 >= 0 && dis[i][j-1] == -1)
+                            dis[i][j-1] = scan+1;
+                        if (j+1 < dis[i].length && dis[i][j+1] == -1)
+                            dis[i][j+1] = scan+1;
+                        needToScan = true;
+                    }
+                }
+            }
+            scan++;
+        } while (needToScan);
+
+        int max = 0;
+        for (int[] di : dis) {
+            for (int i : di) {
+                if (i > max)
+                    max = i;
+            }
         }
-        sum += result.size();
-        return sum;
+        return max == 0 ? -1 : max;
     }
 
     public static void main(String[] args) {
-        System.out.println(new Solution().minimumLengthEncoding(new String[]{"time", "me", "bell"}));
+        System.out.println(new Solution().maxDistance(new int[][]{{1, 1, 1}, {1, 1, 1}, {1, 1, 1}}));
     }
 }
