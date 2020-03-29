635e7192e0fd22a1a2af360f39231402d3f10041
lixinfeng <lixinfeng@dm-ai.cn>
2020-03-29 19:50:25 +0800

    820. 单词的压缩编码

--- src/Solution.java
+++ src/Solution.java
@@ -1,23 +1,41 @@
+import java.util.ArrayList;
+import java.util.List;
+
 class Solution {
-    public boolean hasGroupsSizeX(int[] deck) {
-        int[] count = new int[10000];
-        for (int c: deck)
-            count[c]++;
-
-        int g = -1;
-        for (int i = 0; i < 10000; ++i) {
-            if (count[i] > 0) {
-                if (g == -1)
-                    g = count[i];
-                else
-                    g = gcd(g, count[i]);
-            }
-        }
-
-        return g > 1;
+    private String reverse(String word) {
+        return new StringBuilder(word).reverse().toString();
     }
 
-    public int gcd(int x, int y) {
-        return x == 0 ? y : gcd(y%x, x);
+    public int minimumLengthEncoding(String[] words) {
+        List<String> result = new ArrayList<String>();
+        result.add(reverse(words[0]));
+        for (int i = 1; i < words.length; i++) {
+            boolean in = false;
+            String rw = reverse(words[i]);
+            for (int j = 0; j < result.size(); j++) {
+                if (result.get(j).length() > rw.length()) {
+                    if (result.get(j).substring(0, rw.length()).equals(rw)) {
+                        in = true;
+                        break;
+                    }
+                } else if (rw.substring(0, result.get(j).length()).equals(result.get(j))) {
+                    in = true;
+                    result.set(j, rw);
+                    break;
+                }
+            }
+            if (!in)
+                result.add(rw);
+        }
+        int sum = 0;
+        for (int i = 0; i < result.size(); i++) {
+            sum += result.get(i).length();
+        }
+        sum += result.size();
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Solution().minimumLengthEncoding(new String[]{"time", "me", "bell"}));
     }
 }
