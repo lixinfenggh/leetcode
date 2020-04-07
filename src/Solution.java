acd76232d6f1e99cc116f7db46cf503a73650ac2
lixinfeng <lixinfeng@dm-ai.cn>
2020-04-07 21:17:25 +0800

    面试题 01.07. 旋转矩阵

--- src/Solution.java
+++ src/Solution.java
@@ -1,26 +1,14 @@
 class Solution {
-    public int trap(int[] height) {
-        if (height.length < 3) return 0;
-        int[] leftMax = new int[height.length],
-                rightMax = new int[height.length];
-        int max = 0;
-        for (int i = 1; i < height.length; i++) {
-            if (height[i-1] > max)
-                max = height[i-1];
-            leftMax[i] = max;
+    public void rotate(int[][] matrix) {
+        int wid = matrix.length;
+        for (int i = 0; i < wid/2; i++) {
+            for (int j = 0; j < (wid+1)/2; j++) {
+                int t = matrix[i][j];
+                matrix[i][j] = matrix[wid-j-1][i];
+                matrix[wid-j-1][i] = matrix[wid-i-1][wid-j-1];
+                matrix[wid-i-1][wid-j-1] = matrix[j][wid-i-1];
+                matrix[j][wid-i-1] = t;
+            }
         }
-        max = 0;
-        for (int i = height.length-2; i >= 0; i--) {
-            if (height[i+1] > max)
-                max = height[i+1];
-            rightMax[i] = max;
-        }
-        int result = 0;
-        for (int i = 1; i < height.length; i++) {
-            int w = Math.min(leftMax[i], rightMax[i]);
-            if (height[i] < w)
-                result += (w-height[i]);
-        }
-        return result;
     }
 }
--- src/Test.java
+++ src/Test.java
@@ -1,6 +1,12 @@
 public class Test {
     public static void main(String[] args) {
-        int result = new Solution().trap(new int[]{0,1,0,2,1,0,1,3,2,1,2,1});
-        System.out.println(result);
+        LFUCache cache = new LFUCache( 2 /* capacity (缓存容量) */ );
+
+        cache.put(2, 1);
+        cache.put(2, 2);
+        System.out.println(cache.get(2));
+        cache.put(1, 1);
+        cache.put(4, 1);
+        System.out.println(cache.get(2));
     }
 }
